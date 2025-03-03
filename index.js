76ffa9445eb377cd5939fe14e3ed9a6bf8dbc9fc
Grace Nduta <ndutak5674@gmail.com>
2025-03-03 22:45:28 +0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+//Define the companyName as a cost and set it as "scuber"
+const companyName="Scuber"
+
+//Define mostProfitableNeighbourhood using let and set it to "Chelsea"
+let mostProfitableNeighborhood="Chelsea"
+
+//Define the companyCeo using let and set it to "Susan Smith"
+let companyCeo="Susan Smith"
+anyCeo="Susan Smith"
